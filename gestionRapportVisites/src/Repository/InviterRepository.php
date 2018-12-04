94eaa46649e50aae958493ea5df11fca2e5e5af8
PC-SIO <PC-SIO>
2018-12-04 16:30:57 +0100

    modif style4

--- gestionRapportVisites/src/Repository/InviterRepository.php
+++ gestionRapportVisites/src/Repository/InviterRepository.php
@@ -48,13 +48,5 @@ class InviterRepository extends ServiceEntityRepository
     }
     */
 
-<<<<<<< HEAD
-    /*public function findParPraticien($id)
-    {
-        return $this->
-    }
-    */
-=======
     
->>>>>>> d5e32017ec70dd12fd90305bcee59a2c4ad12efa
 }
