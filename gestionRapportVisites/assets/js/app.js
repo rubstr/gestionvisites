71202cb8d82c898523cada6ccddb1984a27c7743
PIERRONMarc <pierronmarc@outlook.fr>
2018-12-29 01:28:27 +0100

    Ajout de la technologie SASS via webpack Encore

--- gestionRapportVisites/assets/js/app.js
+++ gestionRapportVisites/assets/js/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../css/app.scss');
 //Bulma
 require('bulma/css/bulma.css');
 //Font-awesome
